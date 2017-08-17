7ef75899c576240c5281983e316c90bf95e7b138
milanmenezes <milanmenezes@gmail.com>
2017-08-17 16:12:50 +0530

    Moving cursor to center of screen

--- output.asm
+++ output.asm
@@ -1,32 +1,52 @@
 assume cs:code, ds:data
 data segment
+a db ?
 data ends
 code segment
 start:
 mov ax,data
 mov ds,ax
+
 mov ah,1
 int 21h
 
 mov bl,al
 
+mov dl, 10
+mov ah,2
+int 21h
+
+mov dh, 24
+mov dl, 35
+mov a,bl
+mov bx,0
+mov ah,2
+int 10h
+
+mov bl,a
+
 mov dl,bl
 mov ah,2
 int 21h
+
 mov dl, ':'
 mov ah,2
 int 21h
+
 mov al,bl
 aam
 mov bx,ax
 add bl, 30h
 add bh, 30h
+
 mov dl, bh
 mov ah,2
 int 21h
+
 mov dl, bl
 mov ah,2
 int 21h
+
 mov ah, 4ch
 int 21h
 code ends
